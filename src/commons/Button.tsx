d37e09016daa451f7459278b2f3cded0547ee821
Alex Emanuel Amico <emanuelamicoo@gmail.com>
2023-05-19 11:27:14 -0300

    replaced svg with better one

--- src/commons/Button.tsx
+++ src/commons/Button.tsx
@@ -13,12 +13,18 @@ const Button: FC<ButtonProps> = ({
 }) => {
   return (
     <button
-      className={`flex items-center uppercase px-4 py-1.5 rounded text-whiteText
-       bg-primaryBlue shadow-md transition-colors hover:bg-blue-600 active:bg-blue-700 ${className || ""}`}
+      className={`flex items-center uppercase px-4 py-1.5 rounded text-whiteText bg-primaryBlue shadow-md transition-colors hover:bg-blue-600 active:bg-blue-700 ${
+        className || ""
+      }`}
       {...buttonProps}
     >
       {loading && (
-        <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+        <svg
+          className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
           <circle
             className="opacity-25"
             cx="12"
@@ -30,7 +36,7 @@ const Button: FC<ButtonProps> = ({
           <path
             className="opacity-75"
             fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
           ></path>
         </svg>
       )}
