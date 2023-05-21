7fd81df1053e6daa139a03d40f48d8133555a1ec
elbolaestevez <elbolaestevez@gmail.com>
2023-05-21 16:00:15 -0300

    adding Navbar to the view

--- src/app/Package-In-Progress/page.tsx
+++ src/app/Package-In-Progress/page.tsx
@@ -1,18 +1,22 @@
 import React, { FC } from "react";
 import DescriptionCard from "@/components/DescriptionCard";
+import Navbar from "../../components/Header";
 
 const page: FC = () => {
   return (
-    <div className="w-full flex h-screen justify-center">
-      <DescriptionCard
-        title={"Reparto En Curso"}
-        destination="Amenabar 2356, Caba"
-        packageId="712"
-        recipient="Raul Rodriguez"
-        coordinates="24,25"
-        buttonText="Finalizar"
-      />
-    </div>
+    <>
+      <Navbar />
+      <div className="w-full flex h-screen justify-center">
+        <DescriptionCard
+          title={"Reparto En Curso"}
+          destination="Amenabar 2356, Caba"
+          packageId="712"
+          recipient="Raul Rodriguez"
+          coordinates="24,25"
+          buttonText="Finalizar"
+        />
+      </div>
+    </>
   );
 };
 
