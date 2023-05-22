4f0029b6a4d018f257c82035be26115f09fb6def
Rodriix99 <roescal347@gmail.com>
2023-05-22 00:04:02 -0300

    done

--- src/commons/InputText.tsx
+++ src/commons/InputText.tsx
@@ -22,9 +22,14 @@ const InputText: FC<IinputText> = ({
   ...inputProps
 }) => {
   return (
-    <div className={" min-w-[12rem] h-[4.5rem] relative "}>
+    <div className={" w-11/12 h-[4.5rem] relative "}>
       <div>
-        <label className=" text-yellowText">{label}</label>
+        <label
+          htmlFor={name}
+          className=" mb-1  ml-2 text-xs text-yellowText font-medium border-0"
+        >
+          {label}
+        </label>
       </div>
 
       <input
@@ -33,13 +38,17 @@ const InputText: FC<IinputText> = ({
         id={id}
         name={name}
         type={hidden ? "password" : "text"}
-        className={`text-black w-full h-[3rem] border-b-2 ${
-          error
-            ? "border-b-red-600"
-            : disabled
-            ? "border-b-disableButton"
-            : "border-b-blue-500"
-        } absolute bottom-0 ring-0 outline-0`}
+        className={`text-black ml-2 text-base w-11/12 p-2.5 border-solid bg-inherit
+
+          ${
+            error
+              ? "border-b-red-600"
+              : disabled
+              ? "border-b-disableButton"
+              : "border-b-blue-500"
+          }
+
+         p-2.5 border-b-2 pb-1 pl-0 ring-0 outline-0  mb-6`}
       />
       <div className="h-[3rem]"></div>
       {error ? <h1 className="text-red-600">{error}</h1> : null}
