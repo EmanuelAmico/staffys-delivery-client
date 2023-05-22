0352a38398fd0069ee9085e5db07d2629e085034
Rodriix99 <roescal347@gmail.com>
2023-05-22 16:08:17 -0300

    add base

---
+++ src/app/pending-packages/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Layout from "@/commons/Layout";
+function PendingPackages() {
+  return <Layout className="bg-[#F5F5F5]"></Layout>;
+}
+
+export default PendingPackages;
