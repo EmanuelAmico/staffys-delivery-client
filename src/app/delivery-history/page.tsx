172a7f556c4b5d192e9016e3ba73ee80ee6ea60e
elbolaestevez <elbolaestevez@gmail.com>
2023-05-28 17:12:23 -0300

    page history delivery done

--- src/app/delivery-history/page.tsx
+++ src/app/delivery-history/page.tsx
@@ -1,48 +1,46 @@
 import React, { FC } from "react";
-import DropdownBox from "@/commons/DropdownBox";
+
 import InputText from "@/commons/InputText";
 import Layout from "@/commons/Layout";
 import Button from "@/commons/Button";
 import DeliveryPackageCard from "@/commons/DeliveryPackageCard";
+import { deliveryPackages } from "@/utils/FakeDataDeliveryPackages";
+import IconButton from "@/commons/IconButton";
+import { RiArrowLeftSLine } from "react-icons/ri";
 
 const page: FC = () => {
   return (
-    <Layout className="h-full bg-pageBackground">
-      <div className="flex flex-col gap-6 mb-4">
-        <div className="flex flex-row gap-2">
-          <Button className="mt-8 w-11/12 ">Direccion</Button>
-          <Button className="mt-8 w-11/12 ">Destinatario</Button>
-          <Button className="mt-8 w-11/12 ">ID</Button>
+    <Layout className="h-[93%] bg-pageBackground">
+      <div className="">
+        <IconButton icon={<RiArrowLeftSLine size={40} />} />
+      </div>
+      <div className="flex flex-col gap-6 mb-4 mt-3">
+        <div className="flex flex-row gap-2 ">
+          <Button className="w-11/12 ">Direccion</Button>
+          <Button className="w-11/12 ">Destinatario</Button>
+          <Button className="w-11/12 ">ID</Button>
         </div>
         <InputText
           name="searcher"
-          placeHolder="Inserte su búsqueda"
+          placeholder="Inserte su búsqueda"
           label="Filtrar Pedido"
         />
       </div>
-      <DropdownBox className="h-full" title="Historial de Repartos">
-        <DeliveryPackageCard
-          destination={"Amenabar 2356, Caba"}
-          addressee={"Emanuel Amico"}
-          distance={"26km"}
-          buttonText={"Agregar"}
-          trash={false}
-        />
-        <DeliveryPackageCard
-          destination={"Amenabar 2356, Caba"}
-          addressee={"Emanuel Amico"}
-          distance={"26km"}
-          buttonText={"Agregar"}
-          trash={false}
-        />
-        <DeliveryPackageCard
-          destination={"Amenabar 2356, Caba"}
-          addressee={"Emanuel Amico"}
-          distance={"26km"}
-          buttonText={"Agregar"}
-          trash={false}
-        />
-      </DropdownBox>
+
+      <div className="pt-4 mb-4 px-4 border-t-2 overflow-y-scroll">
+        {deliveryPackages.map((deliveryPackage) => (
+          <>
+            <DeliveryPackageCard
+              className="mb-4"
+              key={deliveryPackage.id}
+              {...deliveryPackage}
+            />
+            {deliveryPackage !== deliveryPackages.at(-1) && (
+              <hr className="mb-4" />
+            )}
+          </>
+        ))}
+      </div>
     </Layout>
   );
 };
