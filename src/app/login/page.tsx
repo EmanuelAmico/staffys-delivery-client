72b8e167aa0e67d9966130567f47ff299949183a
Rafa Mojica <rafael.mojica27@gmail.com>
2023-05-28 13:42:18 -0500

    Change the name TextInput

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Layout from "@/commons/Layout";
 import Button from "@/commons/Button";
 import Link from "@/commons/Link";
-import InputText from "@/commons/InputText";
+import TextInput from "@/commons/InputText";
 import logoFastDelivery from "../../../public/images/logoFastDelivery.png";
 
 const Login: FC = () => {
@@ -18,12 +18,12 @@ const Login: FC = () => {
         />
       </div>
       <form className="pt-16 pb-5">
-        <InputText
+        <TextInput
           label="Usuario"
           name="email"
           placeholder="staffys@gmail.com"
         />
-        <InputText
+        <TextInput
           label="Contraseña"
           name="password"
           placeholder="Contraseña"
