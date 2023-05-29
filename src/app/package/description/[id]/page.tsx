97070274134f91ba1667d963318e0f10f7b16238
Alex Emanuel Amico <74570470+EmanuelAmico@users.noreply.github.com>
2023-05-29 10:49:05 -0300

    Add className to Card in PackageDescription

--- src/app/package/description/[id]/page.tsx
+++ src/app/package/description/[id]/page.tsx
@@ -12,7 +12,7 @@ const page: FC = () => {
       <div className="w-full mt-3">
         <IconButton icon={<RiArrowLeftSLine size={40} />} />
       </div>
-      <Card title="Reparto finalizado">
+      <Card title="Reparto finalizado" className="flex justify-center bg-white w-[90%] mt-8 rounded-lg shadow-lg h-[37rem]">
         <PackageDescription
           destination="Amenabar 2356, Caba"
           packageId="712"
