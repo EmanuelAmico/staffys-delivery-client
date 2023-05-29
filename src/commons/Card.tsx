e89b71c483766f124f6a67ad76f45a7fdb5b3db5
Alex Emanuel Amico <74570470+EmanuelAmico@users.noreply.github.com>
2023-05-29 10:47:04 -0300

    Update Card and add className

--- src/commons/Card.tsx
+++ src/commons/Card.tsx
@@ -2,15 +2,14 @@ import React, { FC } from "react";
 export interface CardProps {
   title: string;
   children: JSX.Element[] | JSX.Element;
+  className?: string;
 }
 
-const Card: FC<CardProps> = ({ title, children }) => {
+const Card: FC<CardProps> = ({ title, children, className }) => {
   return (
-    <div className="flex justify-center bg-white w-[90%] mt-8 rounded-lg shadow-lg h-[37rem] ">
-      <div className=" h-full w-[90%] m-auto  ">
-        <h1 className="text-left pt-4 font-bold text-2xl">{title}</h1>
-        {children}
-      </div>
+    <div className={`p-4 shadow-md rounded ${className || ""}`}>
+      <h1 className="font-bold text-2xl">{title}</h1>
+      {children}
     </div>
   );
 };
