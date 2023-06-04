b9b7e703f51c0d4e2e47c8e2b12fc96bf3d03589
Rafa Mojica <rafael.mojica27@gmail.com>
2023-06-03 19:42:13 -0500

    add favicon without baground

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata = {
   title: "Staffys Delivery",
   description:
     "Easy to use web application that allows you to navigate quickly so you can monitor the operation of the delivery drivers, assign or reassign packages and intervene if necessary.",
+  icons: {
+    favicon: "./favicon.ico",
+  },
 };
 
 export default function RootLayout({
