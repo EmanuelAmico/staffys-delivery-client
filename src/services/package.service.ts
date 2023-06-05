72467ffb69ac7ca2f3688b92ee5b84556eca9676
elbolaestevez <elbolaestevez@gmail.com>
2023-06-05 15:37:36 -0300

    servicios done

---
+++ src/services/package.service.ts
@@ -0,0 +1,34 @@
+import { deliveryHistory } from "../utils/FakeDataDeliveryHistory";
+import { deliveryPackages } from "../utils/FakeDataDeliveryPackages";
+import { deliveryPending } from "../utils/FakeDataDeliveryPending";
+import { waitRandomSeconds } from "../utils/wait";
+
+export class PackageService {
+  static async getPackageById(id: string) {
+    await waitRandomSeconds();
+    return { message: `the package id:${id} has been add` };
+  }
+
+  static async getAvailablePackages() {
+    await waitRandomSeconds();
+    return deliveryPackages;
+  }
+
+  static async getHistoryPackagesById() {
+    await waitRandomSeconds();
+    return deliveryHistory;
+  }
+  static async getPendingPackagesById() {
+    await waitRandomSeconds();
+    return deliveryPending;
+  }
+
+  static async takePackage(id: string) {
+    await waitRandomSeconds();
+    return { message: `the package id:${id} has taken` };
+  }
+  static async deletePackage(id: string) {
+    await waitRandomSeconds();
+    return { message: `the package id:${id} has been deleted` };
+  }
+}
