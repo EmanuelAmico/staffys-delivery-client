479ad855a99d8289dfda92ecd64b213f29e42894
Alex Emanuel Amico <emanuelamicoo@gmail.com>
2023-07-16 20:23:32 -0300

    Add logout to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,46 @@
 import React from "react";
 import Image from "next/image";
+import { SlLogout } from "react-icons/sl";
+import IconButton from "@/commons/IconButton";
+import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/redux/store";
+import { logout } from "@/redux/reducers/user";
 
 const Header = () => {
+  const { push } = useRouter();
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    push("/login");
+  };
+
   return (
-    <div className="flex w-full h-[60px] items-center shadow-md relative">
-      <div>
-        <Image
-          src={"/svg/motorcycle.svg"}
-          alt="Moto"
-          width={20}
-          height={20}
-          className="w-[80px] h-[60px]"
-          priority
-        />
-      </div>
-      <div className="w-full absolute text-center">
-        <p className="text-greyText">
-          FAST
-          <span className="text-yellowText"> DELIVERY</span>
-        </p>
-      </div>
-    </div>
+    <header className="flex justify-between items-center w-full h-[60px] shadow-md relative">
+      <IconButton
+        icon={
+          <Image
+            src={"/svg/motorcycle.svg"}
+            alt="Moto"
+            width={20}
+            height={20}
+            className="w-[80px] h-[60px]"
+            priority
+          />
+        }
+        onClick={() => push("/home")}
+      />
+      <p className="w-full absolute text-center text-greyText -z-10">
+        FAST
+        <span className="text-yellowText"> DELIVERY</span>
+      </p>
+      <IconButton
+        onClick={handleLogout}
+        icon={<SlLogout size={24} />}
+        className="mx-4"
+      />
+    </header>
   );
 };
 
